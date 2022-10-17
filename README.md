00fb9e96e3a56c0fa52ab8df4caea28ffcd9cbf2
Shel Cloyd <shel.cloyd@gmail.com>
2022-10-17 12:59:11 -0700

    styled md file structure

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# README-Generator
+# **Project X**
+
+___
+
+#### **Table of Contents**
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [How to Use](#instructions)
+* [License](#license)
+* [Link to Application](#link)
+* [Contact](#contact)
+
+___
+
+<div id="description"></div>
+
+## **Description**
+Suspendisse nec dignissim lectus. Duis pulvinar consectetur mi, ac rutrum magna tempor vitae. In gravida sed libero mattis gravida. Proin magna enim, ultricies ut dictum sed, rhoncus vestibulum turpis. Ut tempus est commodo, fringilla arcu ac, rhoncus est. Nulla porta nibh nec enim viverra, vitae fringilla nulla aliquet. Donec enim erat, sodales eget sagittis ut, faucibus at turpis. Ut ut aliquam leo. Cras in dui sit amet turpis luctus eleifend. Vivamus faucibus dolor tempus ex mollis mattis. Nam sagittis varius felis. Nulla eu ante non orci rutrum dictum vel vel massa. Vestibulum finibus a orci eu suscipit. Integer vel lectus tempus, scelerisque erat sed, dignissim nisi.
+
+___
+
+<div id="installation"></div>
+
+## **Installation Instructions**
+Suspendisse nec dignissim lectus. Duis pulvinar consectetur mi, ac rutrum magna tempor vitae. In gravida sed libero mattis gravida. Proin magna enim, ultricies ut dictum sed, rhoncus vestibulum turpis. Ut tempus est commodo, fringilla arcu ac, rhoncus est. Nulla porta nibh nec enim viverra, vitae fringilla nulla aliquet. Donec enim erat, sodales eget sagittis ut, faucibus at turpis. Ut ut aliquam leo. Cras in dui sit amet turpis luctus eleifend. Vivamus faucibus dolor tempus ex mollis mattis. Nam sagittis varius felis. Nulla eu ante non orci rutrum dictum vel vel massa. Vestibulum finibus a orci eu suscipit. Integer vel lectus tempus, scelerisque erat sed, dignissim nisi.</div>
+
+___
+
+<div id="usage"></div>
+
+## **Intended Usage**
+Suspendisse nec dignissim lectus. Duis pulvinar consectetur mi, ac rutrum magna tempor vitae. In gravida sed libero mattis gravida. Proin magna enim, ultricies ut dictum sed, rhoncus vestibulum turpis. Ut tempus est commodo, fringilla arcu ac, rhoncus est. Nulla porta nibh nec enim viverra, vitae fringilla nulla aliquet. Donec enim erat, sodales eget sagittis ut, faucibus at turpis. Ut ut aliquam leo. Cras in dui sit amet turpis luctus eleifend. Vivamus faucibus dolor tempus ex mollis mattis. Nam sagittis varius felis. Nulla eu ante non orci rutrum dictum vel vel massa. Vestibulum finibus a orci eu suscipit. Integer vel lectus tempus, scelerisque erat sed, dignissim nisi.
+
+___
+
+<div id="contributions"></div>
+
+## **Contributions**
+Suspendisse nec dignissim lectus. Duis pulvinar consectetur mi, ac rutrum magna tempor vitae. In gravida sed libero mattis gravida. Proin magna enim, ultricies ut dictum sed, rhoncus vestibulum turpis. Ut tempus est commodo, fringilla arcu ac, rhoncus est. Nulla porta nibh nec enim viverra, vitae fringilla nulla aliquet. Donec enim erat, sodales eget sagittis ut, faucibus at turpis. Ut ut aliquam leo. Cras in dui sit amet turpis luctus eleifend. Vivamus faucibus dolor tempus ex mollis mattis. Nam sagittis varius felis. Nulla eu ante non orci rutrum dictum vel vel massa. Vestibulum finibus a orci eu suscipit. Integer vel lectus tempus, scelerisque erat sed, dignissim nisi.
+
+___
+
+<div id="instructions"></div>
+
+## **How to Use Project X**
+Suspendisse nec dignissim lectus. Duis pulvinar consectetur mi, ac rutrum magna tempor vitae. In gravida sed libero mattis gravida. Proin magna enim, ultricies ut dictum sed, rhoncus vestibulum turpis. Ut tempus est commodo, fringilla arcu ac, rhoncus est. Nulla porta nibh nec enim viverra, vitae fringilla nulla aliquet. Donec enim erat, sodales eget sagittis ut, faucibus at turpis. Ut ut aliquam leo. Cras in dui sit amet turpis luctus eleifend. Vivamus faucibus dolor tempus ex mollis mattis. Nam sagittis varius felis. Nulla eu ante non orci rutrum dictum vel vel massa. Vestibulum finibus a orci eu suscipit. Integer vel lectus tempus, scelerisque erat sed, dignissim nisi.
+
+___
+
+<div id="license"></div>
+
+## **License**
+* MIT License
+
+___
+
+<div id="link"></div>
+
+## **Link to Application**
+* https://github.com/shelcloyd/Portfolio.git
+
+___
+
+<div id="contact"></div>
+
+## **Questions?**
+* GitHub: shelcloyd
+* Email:  shel.cloyd@gmail.com
--- index.js
+++ index.js
@@ -1,105 +1,143 @@
-// TODO: Include packages needed for this application
+// packages used for project
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const generateMd = ({title, description, installation, usage, contributions, instructions, license, link, github, contact}) =>
-`# ${title}
-## Description
-> ${description}
+// structure of README.md file to be generated
+const generateMd = ({ title, description, installation, usage, contributions, instructions, license, link, github, contact }) =>
+    `# **${title}**
+
 ___
-## Installation Instructions
-> ${installation}
+
+#### **Table of Contents**
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [How to Use](#instructions)
+* [License](#license)
+* [Link to Application](#link)
+* [Contact](#contact)
+
 ___
-## Intended Usage
-> ${usage}
+
+<div id="description"></div>
+
+## **Description**
+${description}
+
 ___
-## Contributions
-> ${contributions}
+
+<div id="installation"></div>
+
+## **Installation Instructions**
+${installation}</div>
+
 ___
-## How to Use ${title}
-> ${instructions}
+
+<div id="usage"></div>
+
+## **Intended Usage**
+${usage}
+
 ___
-## License
+
+<div id="contributions"></div>
+
+## **Contributions**
+${contributions}
+
+___
+
+<div id="instructions"></div>
+
+## **How to Use ${title}**
+${instructions}
+
+___
+
+<div id="license"></div>
+
+## **License**
 * ${license}
+
 ___
-## Link to Application
+
+<div id="link"></div>
+
+## **Link to Application**
 * ${link}
+
 ___
-## Contact Me
-* ${github}
-* ${contact}
+
+<div id="contact"></div>
+
+## **Questions?**
+* GitHub: ${github}
+* Email:  ${contact}
 `
 
-// TODO: Create an array of questions for user input
-// const questions = [
-    inquirer
+// questions to gather data for file creation
+inquirer
     .prompt([
-    {
-        type: "input",
-        message: "What is your application's name?",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "Please enter a description for your application.",
-        name: "description",
-    },
-    {
-        type: "input",
-        message: "What are the intallation instructions for your application?",
-        name: "installation",
-    },
-    {
-        type: "input",
-        message: "How will your application be used?",
-        name: "usage",
-    },
-    {
-        type: "input",
-        message: "How can users contribute to your application?",
-        name: "contributions",
-    },
-    {
-        type: "input",
-        message: "Please enter instructions for how to test your application.",
-        name: "instructions",
-    },
-    {
-        type: "list",
-        message: "Would you like to add a license to your application?",
-        name: "license",
-        choices: ["MIT License", "Boost Software License 2.0", "Apache License 2.0", "The Unlicense"],
-    },
-    {
-        type: "input",
-        message: "Paste the link to your application here.",
-        name: "link",
-    },
-    {
-        type: "input",
-        message: "Please enter your GitHub username.",
-        name: "github",
-    },
-    {
-        type: "input",
-        message: "Please enter your email address.",
-        name: "contact",
-    },
-])
-.then((data) => {
-    const readMe = generateMd(data);
+        {
+            type: "input",
+            message: "What is your application's name?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "Please enter a description for your application.",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What are the intallation instructions for your application?",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "How will your application be used?",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "How can users contribute to your application?",
+            name: "contributions",
+        },
+        {
+            type: "input",
+            message: "Please enter instructions for how to test your application.",
+            name: "instructions",
+        },
+        {
+            type: "list",
+            message: "Would you like to add a license to your application?",
+            name: "license",
+            choices: ["MIT License", "Boost Software License 1.0", "Apache License 2.0", "The Unlicense"],
+        },
+        {
+            type: "input",
+            message: "Paste the link to your application here.",
+            name: "link",
+        },
+        {
+            type: "input",
+            message: "Please enter your GitHub username.",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "Please enter your email address.",
+            name: "contact",
+        },
+    ])
+    // create file
+    .then((data) => {
+        const readMe = generateMd(data);
 
-    fs.writeFile("README.md", readMe, (err) =>
-    err ? console.error(err) : console.log("You did it!")
-    );
-})
-// ];
+        fs.writeFile("README.md", readMe, (err) =>
+            err ? console.error(err) : console.log("You did it!")
+        );
+    });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
